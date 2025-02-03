41fec10d6bdb87b18dec944a75862844a4da9ce2
Devilking12 <128044704+Devilking12@users.noreply.github.com>
2025-02-04 00:29:48 +0530

    Update bot.py

--- BADMUSIC/core/bot.py
+++ BADMUSIC/core/bot.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2024 by Badhacker98@Github, < https://github.com/Badhacker98 >.
+# Copyright (C) 2024 by RedTeam, < https://github.com/Badhacker98 >.
 # Owner https://t.me/ll_BAD_MUNDA_ll
 
 import asyncio
